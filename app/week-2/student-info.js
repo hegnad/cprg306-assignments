3140724baae2c9d777ce2e0c0ad932077c924701
Hy Dang <hydang@outlook.com>
2024-05-27 20:41:03 -0600

    Week 2 Submission

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+const StudentInfo = () => {
+    return (
+        <div>
+            <h1>Hy Marcel Dang</h1>
+            <Link href="https://github.com/hegnad/cprg306-assignments">My Github Repo</Link>
+        </div>
+    );
+};
+
+export default StudentInfo;
