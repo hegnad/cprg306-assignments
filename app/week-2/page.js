070ecbdfeafb3089c5b45ce798ae719b05f827b3
Hy Dang <hydang@outlook.com>
2024-06-03 00:56:52 -0600

    Added link to go back to Assignments page

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import StudentInfo from './student-info';
+import Link from 'next/link';
 
 export default function Page() {
     return (
         <main>
             <h1>Shopping List</h1>
             <StudentInfo />
+
+            <Link href="/" className="text-blue-500 hover:underline flex justify-center items-center">Go back to Assignments</Link>
+
         </main>
     );
 }
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ItemList from './item-list';
+import Link from 'next/link';
 
 export default function Page() {
+
     return (
+
         <main>
+
             <h1 className="flex justify-center items-center text-lg font-bold">Shopping List</h1>
             <ItemList />
+            <Link className="text-blue-500 hover:underline flex justify-center items-center" href="/">Go back to Assignments</Link>
+
         </main>
+
     );
+
 }
